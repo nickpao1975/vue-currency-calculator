d0c348fbc5f9a8190fa0cc15531aed61381289c3
Georg Brune <gb@gbrune.net>
2022-12-05 09:35:14 +0000

    [repetition] remove counter component from App.vue
    this was just an example, so we can remove this component again to have a clean project.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <script lang="ts">
-import Counter from "@/components/Counter/Counter.vue";
 import CurrencyCalculator from "@/components/CurrencyCalculator/CurrencyCalculator.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  components: { CurrencyCalculator, Counter },
-});
-
-defineComponent({
   components: {
-    // "currency-calculator": CurrencyCalculator,
+    "currency-calculator": CurrencyCalculator,
   },
 });
 </script>
 
 <template>
-  <Counter></Counter>
-<!--  <currency-calculator></currency-calculator>-->
+  <currency-calculator></currency-calculator>
 
 </template>
 
